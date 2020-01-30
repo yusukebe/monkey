7b29d0218d9c5650e8fd2eccf77615222046ddc9
Yusuke Wada <yusuke@kamawada.com>
2020-01-30 19:18:15 +0900

    Implemented '2.6.8 前置演算子'

--- ast/ast.go
+++ ast/ast.go
@@ -139,6 +139,28 @@ func (es *ExpressionStatement) String() string {
 }
 
 // IntegerLiteral
-func (il *IntegerLiteral) expressionNode() {}
+func (il *IntegerLiteral) expressionNode()      {}
 func (il *IntegerLiteral) TokenLiteral() string { return il.Token.Literal }
-func (il *IntegerLiteral) String() string { return il.Token.Literal }
+func (il *IntegerLiteral) String() string       { return il.Token.Literal }
+
+// PrefixExpression
+type PrefixExpression struct {
+	Token    token.Token // 前置トークン、例えば「!」
+	Operator string
+	Right    Expression
+}
+
+func (pe *PrefixExpression) expressionNode() {}
+func (pe *PrefixExpression) TokenLiteral() string {
+	return pe.Token.Literal
+}
+func (pe *PrefixExpression) String() string {
+	var out bytes.Buffer
+
+	out.WriteString("(")
+	out.WriteString(pe.Operator)
+	out.WriteString(pe.Right.String())
+	out.WriteString(")")
+
+	return out.String()
+}
