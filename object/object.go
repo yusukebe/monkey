f33a52c68de50e5544f03066d0e064b542b1f4c1
Yusuke Wada <yusuke@kamawada.com>
2020-02-11 08:30:29 +0900

    "3.4.1 オブジェクトシステムの基礎" を実装

---
+++ object/object.go
@@ -0,0 +1,55 @@
+package object
+
+import (
+	"fmt"
+)
+
+type ObjectType string
+
+const (
+	INTEGER_OBJ = "INTEGER"
+	BOOLEN_OBJ  = "BOOLEAN"
+	NULL_OBJ    = "NULL"
+)
+
+type Object interface {
+	Type() ObjectType
+	Inspect() string
+}
+
+// Integer
+type Integer struct {
+	Value int64
+}
+
+func (i *Integer) Type() ObjectType {
+	return INTEGER_OBJ
+}
+
+func (i *Integer) Inspect() string {
+	fmt.Sprintf("%d", i.Value)
+}
+
+// Boolean
+type Boolean struct {
+	Value bool
+}
+
+func (b *Boolean) Type() ObjectType {
+	return BOOLEN_OBJ
+}
+
+func (b *Boolean) Inspect() string {
+	fmt.Sprintf("%t", b.Value)
+}
+
+// Null
+type Null struct{}
+
+func (n *Null) Type() ObjectType {
+	return NULL_OBJ
+}
+
+func (n *Null) Inspect() string {
+	return "null"
+}
