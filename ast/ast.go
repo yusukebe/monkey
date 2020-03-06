8925d34fcb7251711e6c929fc79dc0b7f0b4aa04
Yusuke Wada <yusuke@kamawada.com>
2020-03-06 15:01:53 +0900

    "4.5.2 ハッシュリテラルの構文解析" を実装

--- ast/ast.go
+++ ast/ast.go
@@ -358,3 +358,28 @@ func (ie *IndexExpression) String() string {
 
 	return out.String()
 }
+
+// Hash
+type HashLiteral struct {
+	Token token.Token // '{' トークン
+	Pairs map[Expression]Expression
+}
+
+func (hl *HashLiteral) expressionNode() {}
+func (hl *HashLiteral) TokenLiteral() string {
+	return hl.Token.Literal
+}
+func (hl *HashLiteral) String() string {
+	var out bytes.Buffer
+
+	pairs := []string{}
+	for key, value := range hl.Pairs {
+		pairs = append(pairs, key.String()+":"+value.String())
+	}
+
+	out.WriteString("{")
+	out.WriteString(strings.Join(pairs, ", "))
+	out.WriteString("}")
+
+	return out.String()
+}
